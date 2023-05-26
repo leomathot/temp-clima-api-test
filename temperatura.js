f237068bb08dcc0f55026bf8e9b8aa4200f00788
Leo Mathot <113390921+leomathot@users.noreply.github.com>
2023-05-26 20:51:24 -0300

    Se cambian parametros en el API fetch

--- temperatura.js
+++ temperatura.js
@@ -1,4 +1,4 @@
-let ciudades = document.querySelectorAll("#ciudad")
+let ciudades = document.querySelectorAll(".ciudad")
 
 ciudades.forEach(elem => {
     elem.addEventListener("click", () => {
@@ -28,12 +28,12 @@ ciudades.forEach(elem => {
                 latitud = -31.40
                 longitud = -64.17
         }
-        divDatos = document.querySelector('#datos')
-        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitud}&longitude=${longitud}&hourly=temperature_2m`)
+        divDatos = document.querySelector('#temp-cuidad-resp')
+        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitud}&longitude=${longitud}&current_weather=true`)
             .then(res=>res.json())
             .then(data=>{
                 console.log(data)
-                let temp = data.hourly.temperature_2m[0]
+                let temp = data.current_weather.temperature
                 divDatos.innerHTML=`
                     <h3>Temperatura en ${ciudad}: ${temp}º<h3>`    
                 })
